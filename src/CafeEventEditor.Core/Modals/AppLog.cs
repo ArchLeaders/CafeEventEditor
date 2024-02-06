fe731065d668cd9eda1f772f30b39b5d240980a7
ArchLeaders <archleadership28@gmail.com>
2024-02-06 14:01:53 -0800

    Impl logging and app status modals

---
+++ src/CafeEventEditor.Core/Modals/AppLog.cs
@@ -0,0 +1,74 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Diagnostics;
+
+namespace CafeEventEditor.Core.Modals;
+
+public enum LogLevel
+{
+    Default,
+    Info,
+    Debug,
+    Warning,
+    Error
+}
+
+public partial class AppLog : ObservableObject
+{
+    static AppLog()
+    {
+        Trace.Listeners.Add(new ConsoleTraceListener());
+    }
+
+    public static void Write(string message, LogLevel level)
+    {
+        Trace.WriteLine(level == LogLevel.Default ? message : $"[{level}] {message}");
+    }
+
+    public static void WriteError(Exception ex)
+    {
+        Trace.WriteLine($"""
+            [{LogLevel.Error}] {ex.Message}
+            {ex}
+            """);
+    }
+
+    [ObservableProperty]
+    private string _message;
+
+    [ObservableProperty]
+    private DateTime _date = DateTime.UtcNow;
+
+    [ObservableProperty]
+    private LogLevel _logLevel;
+
+    public AppLog(string message)
+    {
+        int startIndex = message.IndexOf('[');
+        int endIndex = message.IndexOf(']');
+
+        if (startIndex > -1 && endIndex > -1 && Enum.TryParse(message[++startIndex..endIndex], ignoreCase: true, out LogLevel logLevel)) {
+            _message = message[++endIndex..];
+            _logLevel = logLevel;
+        }
+        else {
+            _message = message;
+        }
+    }
+
+    public AppLog(string message, LogLevel logLevel)
+    {
+        _message = message;
+        _logLevel = logLevel;
+    }
+
+    public string ToMarkdown()
+    {
+        return $"""
+            ### `[{LogLevel}]` `[{Date}]`
+
+            ```
+            {Message}
+            ```
+            """;
+    }
+}
