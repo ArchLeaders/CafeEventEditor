0d2494c30edcc42fccab84790f2ff6df9938185e
ArchLeaders <archleadership28@gmail.com>
2024-02-06 17:00:38 -0800

    Update namespaces

--- src/CafeEventEditor/App.axaml.cs
+++ src/CafeEventEditor/App.axaml.cs
@@ -4,6 +4,7 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
 using CafeEventEditor.Builders;
+using CafeEventEditor.Components.Menus;
 using CafeEventEditor.Views;
 using System.Reflection;
 
