811e65e09f0564d70ea04364b5fc8cfe4b785498
ArchLeaders <archleadership28@gmail.com>
2024-02-19 00:59:39 -0800

    Stub EntryPoint node

---
+++ src/CafeEventEditor/ViewModels/Nodes/EntryPointNode.cs
@@ -0,0 +1,35 @@
+﻿using Avalonia.NodeEditor.Core;
+using Avalonia.NodeEditor.Core.Mvvm.Extensions;
+using Avalonia.NodeEditor.Mvvm;
+using CafeEventEditor.Views.Nodes;
+
+namespace CafeEventEditor.ViewModels.Nodes;
+
+public partial class EntryPointNode : ObservableNode, INodeTemplateProvider
+{
+    private const string DEFAULT_NAME = "Action Event";
+
+    public static INodeTemplate Template { get; } = new ObservableNodeTemplate(() => new EntryPointNode()) {
+        Preview = new EntryPointNode(DEFAULT_NAME),
+        Title = DEFAULT_NAME
+    };
+
+    public EntryPointNode() : this(DEFAULT_NAME)
+    {   
+    }
+
+    public EntryPointNode(string name)
+    {
+        Name = name;
+        EntryPointNodeView view = new() {
+            DataContext = this
+        };
+
+        Content = view;
+        Width = view.DesiredSize.Width;
+        Height = view.DesiredSize.Height;
+
+        this.AddPin(Width / 2, 0, 10, 10, PinAlignment.Top, "Input");
+        this.AddPin(Width / 2, Height, 10, 10, PinAlignment.Bottom, "Output");
+    }
+}
---
+++ src/CafeEventEditor/Views/Nodes/EntryPointNodeView.axaml.cs
@@ -0,0 +1,10 @@
+using Avalonia.Controls;
+
+namespace CafeEventEditor.Views.Nodes;
+public partial class EntryPointNodeView : UserControl
+{
+    public EntryPointNodeView()
+    {
+        InitializeComponent();
+    }
+}
