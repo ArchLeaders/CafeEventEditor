4a640850e6963ae8073862abd7382879f903a6a1
ArchLeaders <archleadership28@gmail.com>
2024-02-19 00:58:53 -0800

    Impl static graph view

--- src/CafeEventEditor/ViewModels/FlowchartViewModel.cs
+++ src/CafeEventEditor/ViewModels/FlowchartViewModel.cs
@@ -1,4 +1,6 @@
-﻿using BfevLibrary.Core;
+﻿using AvaloniaGraphControl;
+using BfevLibrary.Core;
+using CafeEventEditor.Services;
 using CafeEventEditor.Components;
 using CafeEventEditor.Components.Models;
 using CafeEventEditor.Core.Components;
@@ -16,10 +18,23 @@ public partial class FlowchartViewModel : Document
     [ObservableProperty]
     private Flowchart? _flowchart;
 
+    [ObservableProperty]
+    private Graph _flowchartGraph = new();
+
     public FlowchartViewModel(string file) : base(Path.GetFileName(file))
     {
         Handle = CafeLoadManager.LoadFromFile(file);
         Flowchart = Handle.Bfev.Flowchart;
         Content = new FlowchartView(this);
+    }
+
+    partial void OnFlowchartChanged(Flowchart? value)
+    {
+        if (value is null) {
+            return;
+        }
+
+        FlowchartGraphFactory factory = new(value);
+        FlowchartGraph = factory.Build();
     }
 }
