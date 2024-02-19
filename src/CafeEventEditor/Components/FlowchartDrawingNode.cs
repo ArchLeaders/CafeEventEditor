2d0d31d9a3a2adfb2938875d1f535441e2c0c2fd
ArchLeaders <archleadership28@gmail.com>
2024-02-19 00:59:21 -0800

    Impl FlowchartDrawingNode

---
+++ src/CafeEventEditor/Components/FlowchartDrawingNode.cs
@@ -0,0 +1,46 @@
+﻿using Avalonia.NodeEditor.Core;
+using Avalonia.NodeEditor.Mvvm;
+using BfevLibrary.Core;
+
+namespace CafeEventEditor.Components;
+
+public partial class FlowchartDrawingNode : ObservableDrawingNode
+{
+    public FlowchartDrawingNode(Flowchart flowchart)
+    {
+        Name = flowchart.Name;
+        Height = 1000;
+        Width = 1000;
+
+        GenerateNodes(flowchart);
+    }
+
+    private void GenerateNodes(Flowchart flowchart)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ToFlowchart()
+    {
+        ArgumentNullException.ThrowIfNull(Name);
+
+        // Flowchart flowchart = new(Name);
+
+        // TODO: Convert the node tree to Events and EntryPoints
+        throw new NotImplementedException();
+    }
+
+    public override bool CanConnectPin(IPin pin)
+    {
+        bool isConnecting = _editor.IsConnectorMoving();
+        if (pin.Name is "Input") {
+            return isConnecting;
+        }
+
+        if (pin.Parent is INode node && node.Name == "Fork") {
+            return true;
+        }
+
+        return base.CanConnectPin(pin);
+    }
+}
---
+++ src/CafeEventEditor/Services/FlowchartNodeFactory.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.NodeEditor.Core;
+using BfevLibrary.Core;
+using CafeEventEditor.Components;
+using CafeEventEditor.ViewModels.Nodes;
+
+namespace CafeEventEditor.Services;
+
+public class FlowchartNodeFactory(Flowchart flowchart) : INodeFactory
+{
+    private readonly Flowchart _flowchart = flowchart;
+
+    public IDrawingNode CreateDrawing(string? name = null)
+    {
+        return new FlowchartDrawingNode(_flowchart) {
+            
+        };
+    }
+
+    public IList<INodeTemplate> CreateTemplates()
+    {
+        return [];
+    }
+}
