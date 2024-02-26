3781f2ca22e877142fcf8eb1f1f84104752751b6
ArchLeaders <archleadership28@gmail.com>
2024-02-25 17:13:26 -0800

    Udpate entry point view/viewmodel

--- src/CafeEventEditor/ViewModels/Nodes/EntryPointNode.cs
+++ src/CafeEventEditor/ViewModels/Nodes/EntryPointNode.cs
@@ -1,13 +1,14 @@
 ﻿using Avalonia.NodeEditor.Core;
 using Avalonia.NodeEditor.Core.Mvvm.Extensions;
 using Avalonia.NodeEditor.Mvvm;
+using BfevLibrary.Core;
 using CafeEventEditor.Views.Nodes;
 
 namespace CafeEventEditor.ViewModels.Nodes;
 
 public partial class EntryPointNode : ObservableNode, INodeTemplateProvider
 {
-    private const string DEFAULT_NAME = "Action Event";
+    private const string DEFAULT_NAME = "Entry Point";
 
     public static INodeTemplate Template { get; } = new ObservableNodeTemplate(() => new EntryPointNode()) {
         Preview = new EntryPointNode(DEFAULT_NAME),
@@ -15,7 +16,7 @@ public partial class EntryPointNode : ObservableNode, INodeTemplateProvider
     };
 
     public EntryPointNode() : this(DEFAULT_NAME)
-    {   
+    {
     }
 
     public EntryPointNode(string name)
@@ -26,10 +27,14 @@ public partial class EntryPointNode : ObservableNode, INodeTemplateProvider
         };
 
         Content = view;
-        Width = view.DesiredSize.Width;
-        Height = view.DesiredSize.Height;
+        Width = view.Width;
+        Height = view.Height;
 
-        this.AddPin(Width / 2, 0, 10, 10, PinAlignment.Top, "Input");
-        this.AddPin(Width / 2, Height, 10, 10, PinAlignment.Bottom, "Output");
+        this.AddPin(Width / 2, Height - view.Padding.Bottom, 10, 10, PinAlignment.Bottom, "Output");
+    }
+
+    public EntryPoint GetEntryPoint()
+    {
+        throw new NotImplementedException();
     }
 }
