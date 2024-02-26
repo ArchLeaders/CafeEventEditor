2157557c37af08a7b47bb67e94c44cd5eddbcb8e
ArchLeaders <archleadership28@gmail.com>
2024-02-25 17:14:58 -0800

    Update NodeFactory template

--- src/CafeEventEditor/Services/FlowchartNodeFactory.cs
+++ src/CafeEventEditor/Services/FlowchartNodeFactory.cs
@@ -1,23 +1,24 @@
 ﻿using Avalonia.NodeEditor.Core;
-using BfevLibrary.Core;
-using CafeEventEditor.Components;
 using CafeEventEditor.ViewModels.Nodes;
 
 namespace CafeEventEditor.Services;
 
-public class FlowchartNodeFactory(Flowchart flowchart) : INodeFactory
+public class FlowchartNodeFactory : INodeFactory
 {
-    private readonly Flowchart _flowchart = flowchart;
-
     public IDrawingNode CreateDrawing(string? name = null)
     {
-        return new FlowchartDrawingNode(_flowchart) {
-            
-        };
+        throw new NotImplementedException();
     }
 
     public IList<INodeTemplate> CreateTemplates()
     {
-        return [];
+        return [
+            EntryPointNode.Template,
+            ActionEventNode.Template,
+            ForkEventNode.Template,
+            JoinEventNode.Template,
+            SubflowEventNode.Template,
+            SwitchEventNode.Template,
+        ];
     }
 }
