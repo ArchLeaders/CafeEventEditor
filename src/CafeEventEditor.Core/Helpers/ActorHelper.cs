0a3cc4488e79ca9f4ef01e78f9d5944be12a863f
ArchLeaders <archleadership28@gmail.com>
2024-02-25 17:17:09 -0800

    Add Actor/Event collection helpers

---
+++ src/CafeEventEditor.Core/Helpers/ActorHelper.cs
@@ -0,0 +1,42 @@
+﻿using BfevLibrary.Core;
+using BfevLibrary.Core.Collections;
+
+namespace CafeEventEditor.Core.Helpers;
+
+public class ActorHelper
+{
+    public ActorList Actors { get; } = [];
+
+    public short GetActorIndex(Actor actor)
+    {
+        int index = Actors.IndexOf(actor);
+        if (index == -1) {
+            index = Actors.Count;
+            Actors.Add(actor);
+        }
+
+        return Convert.ToInt16(index);
+    }
+
+    public static short GetActorActionIndex(Actor actor, string action)
+    {
+        int index = actor.Actions.IndexOf(action);
+        if (index == -1) {
+            index = actor.Actions.Count;
+            actor.Actions.Add(action);
+        }
+
+        return Convert.ToInt16(index);
+    }
+
+    public static short GetActorQueryIndex(Actor actor, string query)
+    {
+        int index = actor.Queries.IndexOf(query);
+        if (index == -1) {
+            index = actor.Queries.Count;
+            actor.Queries.Add(query);
+        }
+
+        return Convert.ToInt16(index);
+    }
+}
---
+++ src/CafeEventEditor.Core/Helpers/EventHelper.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia.NodeEditor.Core;
+using BfevLibrary.Core;
+using BfevLibrary.Core.Collections;
+using CafeEventEditor.Core.Modals;
+
+namespace CafeEventEditor.Core.Helpers;
+
+public class EventHelper
+{
+    public EventList Events { get; } = [];
+    public Dictionary<IEventNode, int> Indices { get; } = [];
+
+    public short GetEventIndex(Event @event, INode? node)
+    {
+        if (node is not IEventNode eventNode) {
+            return -1;
+        }
+
+        if (!Indices.TryGetValue(eventNode, out int index)) {
+            Indices[eventNode] = index = Events.Count;
+            Events.Add(@event);
+        }
+
+        return Convert.ToInt16(index);
+    }
+}
