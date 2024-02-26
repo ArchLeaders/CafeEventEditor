327631bb3a0974b6e9a417bcbf3097401cfd514c
ArchLeaders <archleadership28@gmail.com>
2024-02-25 17:14:12 -0800

    Create JoinEventNode

---
+++ src/CafeEventEditor/ViewModels/Nodes/JoinEventNode.cs
@@ -0,0 +1,35 @@
+﻿using Avalonia.NodeEditor.Core;
+using Avalonia.NodeEditor.Core.Mvvm.Extensions;
+using Avalonia.NodeEditor.Mvvm;
+using CafeEventEditor.Views.Nodes;
+
+namespace CafeEventEditor.ViewModels.Nodes;
+
+public class JoinEventNode : ObservableNode, INodeTemplateProvider
+{
+    private const string DEFAULT_NAME = "Join Event";
+
+    public static INodeTemplate Template { get; } = new ObservableNodeTemplate(() => new JoinEventNode()) {
+        Preview = new JoinEventNode(DEFAULT_NAME),
+        Title = DEFAULT_NAME
+    };
+
+    public JoinEventNode() : this(DEFAULT_NAME)
+    {
+    }
+
+    public JoinEventNode(string name)
+    {
+        Name = name;
+        JoinEventNodeView view = new() {
+            DataContext = this
+        };
+
+        Content = view;
+        Width = view.Width;
+        Height = view.Height;
+
+        this.AddPin(Width / 2, view.Padding.Top, 10, 10, PinAlignment.Top, "Input");
+        this.AddPin(Width / 2, Height- view.Padding.Bottom, 10, 10, PinAlignment.Bottom, "Output");
+    }
+}
---
+++ src/CafeEventEditor/Views/Nodes/JoinEventNodeView.axaml.cs
@@ -0,0 +1,10 @@
+using Avalonia.Controls;
+
+namespace CafeEventEditor.Views.Nodes;
+public partial class JoinEventNodeView : UserControl
+{
+    public JoinEventNodeView()
+    {
+        InitializeComponent();
+    }
+}
