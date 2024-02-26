7e9efc23f8adfaceea6c4a9206be129ec2b34bbe
ArchLeaders <archleadership28@gmail.com>
2024-02-25 17:12:45 -0800

    Create IEventNode interface

---
+++ src/CafeEventEditor.Core/Modals/IEventNode.cs
@@ -0,0 +1,9 @@
+﻿using BfevLibrary.Core;
+using CafeEventEditor.Core.Helpers;
+
+namespace CafeEventEditor.Core.Modals;
+
+public interface IEventNode
+{
+    public Event Append(EventHelper events, ActorHelper actors);
+}
