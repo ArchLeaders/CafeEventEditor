21bb1e67dbbf35614f3aabfc20e644c86f931ba2
ArchLeaders <archleadership28@gmail.com>
2024-02-26 21:23:04 -0800

    Impl ActionEventNode editor

--- src/CafeEventEditor/ViewModels/Nodes/ActionEventNode.cs
+++ src/CafeEventEditor/ViewModels/Nodes/ActionEventNode.cs
@@ -2,14 +2,18 @@
 using Avalonia.NodeEditor.Core.Mvvm.Extensions;
 using Avalonia.NodeEditor.Mvvm;
 using BfevLibrary.Core;
+using CafeEventEditor.Components;
 using CafeEventEditor.Core.Converters;
 using CafeEventEditor.Core.Helpers;
 using CafeEventEditor.Core.Modals;
 using CafeEventEditor.Views.Nodes;
 using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 
 namespace CafeEventEditor.ViewModels.Nodes;
 
+[DebuggerDisplay("{Name} (Action), Actor = {Actor?.Name}, Action = {Action}")]
 public partial class ActionEventNode : ObservableNode, INodeTemplateProvider, IEventNode
 {
     private const string DEFAULT_NAME = "Action Event";
@@ -18,6 +22,18 @@ public partial class ActionEventNode : ObservableNode, INodeTemplateProvider, IE
         Preview = new ActionEventNode(DEFAULT_NAME),
         Title = DEFAULT_NAME
     };
+
+    public ObservableCollection<Actor>? Actors {
+        get {
+            if (Parent is FlowchartDrawingNode drawing) {
+                return drawing.Actors;
+            }
+
+            return [];
+        }
+    }
+
+    public string Info => ToString();
 
     [ObservableProperty]
     private Actor? _actor;
@@ -46,6 +62,12 @@ public partial class ActionEventNode : ObservableNode, INodeTemplateProvider, IE
 
         this.AddPin(Width / 2, view.Padding.Top, 10, 10, PinAlignment.Top, "Input");
         this.AddPin(Width / 2, Height- view.Padding.Bottom, 10, 10, PinAlignment.Bottom, "Output");
+
+        PropertyChanged += (s, e) => {
+            if (e.PropertyName is not nameof(Info)) {
+                OnPropertyChanged(nameof(Info));
+            }
+        };
     }
 
     public Event Append(EventHelper events, ActorHelper actors)
--- src/CafeEventEditor/Views/Nodes/ActionEventNodeView.axaml.cs
+++ src/CafeEventEditor/Views/Nodes/ActionEventNodeView.axaml.cs
@@ -1,4 +1,10 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Styling;
+using AvaloniaEdit;
+using AvaloniaEdit.TextMate;
+using CafeEventEditor.ViewModels.Nodes;
+using TextMateSharp.Grammars;
 
 namespace CafeEventEditor.Views.Nodes;
 public partial class ActionEventNodeView : UserControl
@@ -6,5 +12,27 @@ public partial class ActionEventNodeView : UserControl
     public ActionEventNodeView()
     {
         InitializeComponent();
+    }
+
+    private void TextEditor_Initialized(object? sender, EventArgs e)
+    {
+        if (sender is not TextEditor editor) {
+            return;
+        }
+
+        if (DataContext is ActionEventNode node) {
+            editor.Text = node.Parameters;
+
+            editor.TextChanged += (s, e) => {
+                node.Parameters = editor.Text;
+            };
+        }
+
+        RegistryOptions registryOptions = new(Application.Current?.ActualThemeVariant == ThemeVariant.Dark
+            ? ThemeName.DarkPlus : ThemeName.LightPlus
+        );
+
+        TextMate.Installation textMateInstallation = editor.InstallTextMate(registryOptions);
+        textMateInstallation.SetGrammar("source.yaml");
     }
 }
