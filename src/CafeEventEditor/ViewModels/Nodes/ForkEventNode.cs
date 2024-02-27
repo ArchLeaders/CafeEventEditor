40e8a4b9d88e72e27389fd9f2c2d8126c37f15d1
ArchLeaders <archleadership28@gmail.com>
2024-02-27 11:39:35 -0800

    Stub fork/join event vm

--- src/CafeEventEditor/ViewModels/Nodes/ForkEventNode.cs
+++ src/CafeEventEditor/ViewModels/Nodes/ForkEventNode.cs
@@ -1,11 +1,14 @@
 ﻿using Avalonia.NodeEditor.Core;
 using Avalonia.NodeEditor.Core.Mvvm.Extensions;
 using Avalonia.NodeEditor.Mvvm;
+using BfevLibrary.Core;
+using CafeEventEditor.Core.Helpers;
+using CafeEventEditor.Core.Modals;
 using CafeEventEditor.Views.Nodes;
 
 namespace CafeEventEditor.ViewModels.Nodes;
 
-public class ForkEventNode : ObservableNode, INodeTemplateProvider
+public class ForkEventNode : ObservableNode, INodeTemplateProvider, IEventNode
 {
     private const string DEFAULT_NAME = "Fork Event";
 
@@ -13,6 +16,10 @@ public class ForkEventNode : ObservableNode, INodeTemplateProvider
         Preview = new ForkEventNode(DEFAULT_NAME),
         Title = DEFAULT_NAME
     };
+
+    public string Info => $"""
+        Name: {Name ?? "~"}
+        """;
 
     public ForkEventNode() : this(DEFAULT_NAME)
     {
@@ -31,5 +38,12 @@ public class ForkEventNode : ObservableNode, INodeTemplateProvider
 
         this.AddPin(Width / 2, view.Padding.Top, 10, 10, PinAlignment.Top, "Input");
         this.AddPin(Width / 2, Height- view.Padding.Bottom, 10, 10, PinAlignment.Bottom, "Output");
+    }
+
+    public Event Append(EventHelper events, ActorHelper actors)
+    {
+        ForkEvent forkEvent = new(Name ?? string.Empty);
+
+        throw new NotImplementedException();
     }
 }
--- src/CafeEventEditor/ViewModels/Nodes/JoinEventNode.cs
+++ src/CafeEventEditor/ViewModels/Nodes/JoinEventNode.cs
@@ -1,11 +1,14 @@
 ﻿using Avalonia.NodeEditor.Core;
 using Avalonia.NodeEditor.Core.Mvvm.Extensions;
 using Avalonia.NodeEditor.Mvvm;
+using BfevLibrary.Core;
+using CafeEventEditor.Core.Helpers;
+using CafeEventEditor.Core.Modals;
 using CafeEventEditor.Views.Nodes;
 
 namespace CafeEventEditor.ViewModels.Nodes;
 
-public class JoinEventNode : ObservableNode, INodeTemplateProvider
+public class JoinEventNode : ObservableNode, INodeTemplateProvider, IEventNode
 {
     private const string DEFAULT_NAME = "Join Event";
 
@@ -13,6 +16,10 @@ public class JoinEventNode : ObservableNode, INodeTemplateProvider
         Preview = new JoinEventNode(DEFAULT_NAME),
         Title = DEFAULT_NAME
     };
+
+    public string Info => $"""
+        Name: {Name ?? "~"}
+        """;
 
     public JoinEventNode() : this(DEFAULT_NAME)
     {
@@ -31,5 +38,10 @@ public class JoinEventNode : ObservableNode, INodeTemplateProvider
 
         this.AddPin(Width / 2, view.Padding.Top, 10, 10, PinAlignment.Top, "Input");
         this.AddPin(Width / 2, Height- view.Padding.Bottom, 10, 10, PinAlignment.Bottom, "Output");
+    }
+
+    public Event Append(EventHelper events, ActorHelper actors)
+    {
+        throw new NotImplementedException();
     }
 }
