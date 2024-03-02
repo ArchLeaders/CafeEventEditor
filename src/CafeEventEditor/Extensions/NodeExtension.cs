65938578e3c7e1f738c148e61735535d03c6ebac
ArchLeaders <archleadership28@gmail.com>
2024-03-01 16:37:08 -0800

    Create NodeExtension

---
+++ src/CafeEventEditor/Extensions/NodeExtension.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.NodeEditor.Core;
+
+namespace CafeEventEditor.Extensions;
+
+public static class NodeExtension
+{
+    public static IPin GetFirstPin(this INode node)
+    {
+        ArgumentNullException.ThrowIfNull(node.Pins);
+        return node.Pins[0];
+    }
+
+    public static IPin GetLastPin(this INode node)
+    {
+        ArgumentNullException.ThrowIfNull(node.Pins);
+        return node.Pins[^1];
+    }
+}
